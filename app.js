cd4798feae0c97105aee63902232f0ed420ec4e2
Yao <capslock0921@gmail.com>
2020-07-27 22:41:12 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser')
 const app = express()
 const Todo = require('./models/todo')
 const methodOverride = require('method-override')
+const routes = require('./routes') // 引用路由器
 // 設定連線到 mongoDB://[資料庫帳號]:[資料庫密碼]@[MongoDB位置]:[port]/[資料庫名稱] 
 mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
 
@@ -26,73 +27,8 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(methodOverride('_method'))
 
-app.get('/', (req, res) => {
-  Todo.find() // 取出 Todo model 裡的所有資料  
-    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
-    .sort({ _id: 'asc' }) // desc 降冪
-    .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
-    .catch(error => console.error(error)) // 錯誤處理
-})
-
-app.get('/todos/new', (req, res) => {
-  return res.render('new')
-})
-
-app.post('/todos', (req, res) => {
-  const name = req.body.name // 從 req.body 拿出表單裡的 name 資料
-  // 呼叫Todo物件，直接新增資料
-  return Todo.create({ name }) // 存入資料庫
-    .then(() => res.redirect('/')) // 新增完成後導回首頁
-    .catch(error => console.log(error))
-
-  // 從Todo產生一個實例
-  // const todo = new Todo({ name }) 
-  // return todo.save() 將該實例存入資料庫
-  //   .then(() => res.redirect('/))
-  //   .catch(error => console,log(error))
-})
-
-app.get('/todos/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .lean()
-    .then((todo) => res.render('detail', { todo }))
-    .catch(error => console.log(error))
-})
-
-app.get('/todos/:id/edit', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .lean()
-    .then((todo) => res.render('edit', { todo }))
-    .catch(error => console.log(error))
-})
-
-app.put('/todos/:id', (req, res) => {
-  const id = req.params.id
-  const { name, isDone } = req.body
-  return Todo.findById(id)
-    .then(todo => {
-      todo.name = name
-      todo.isDone = isDone === 'on'
-      // if (isDone === 'on') {
-      //  todo.isDone = true
-      // } else {
-      //   todo.isDone = false
-      // }
-      return todo.save()
-    })
-    .then(() => res.redirect(`/todos/${id}`))
-    .catch(error => console.log(error))
-})
-
-app.delete('/todos/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
-    .then(todo => todo.remove())
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
+// 將 request 導入路由器
+app.use(routes)
 
 
 app.listen(3000, () => {
